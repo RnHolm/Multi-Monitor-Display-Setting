0aca5957ca8eb3433f9293f223d637881a1857ec
Ryan Holm <ryanholm@uiowa.edu>
2019-02-18 09:51:14 -0600

    added comments

--- Multiple Display Settings/Multiple Display Settings/Form1.vb
+++ Multiple Display Settings/Multiple Display Settings/Form1.vb
@@ -1,9 +1,11 @@
 ﻿Public Class Form1
+    'need to use process.start to run command prompt (might help?)
     Private Sub ButtonExt_Click(sender As Object, e As EventArgs) Handles ButtonExt.Click
+        'changes the display to extend 
 
     End Sub
 
     Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
-
+        'changes the display to duplicate 
     End Sub
 End Class
