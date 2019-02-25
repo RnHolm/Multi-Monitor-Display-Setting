3611b92d173cd0efababf81b6433cd034e3b97d7
Ryan Holm <ryanholm@uiowa.edu>
2019-02-25 15:27:46 -0600

    added the processes for each button (they are bugged at the momment). added explinations on what each button does in the GUI. created a test button to test out the command line and explore options to fix the other buttons

--- Multiple Display Settings/Multiple Display Settings/Form1.vb
+++ Multiple Display Settings/Multiple Display Settings/Form1.vb
@@ -3,10 +3,30 @@
     Private Sub ButtonExt_Click(sender As Object, e As EventArgs) Handles ButtonExt.Click
         'changes the display to extend 
         'Switches to "Extend" Display Immediately: %windir%\System32\DisplaySwitch.exe /extend
+        Process.Start("DisplaySwitch", "/extend")
+        MsgBox("the screen has extended")
+
+
     End Sub
 
     Private Sub ButtonDup_Click(sender As Object, e As EventArgs) Handles ButtonDup.Click
         'changes the display to duplicate 
         'Switches to "Duplicate" Display Immediately: %windir%\System32\DisplaySwitch.exe /clone
+        Process.Start("DisplaySwitch", "/clone")
+        MsgBox("the screen has extended")
+    End Sub
+
+    Private Sub ButtonTest_Click(sender As Object, e As EventArgs) Handles ButtonTest.Click
+        'Process.Start("ping", "google.com")
+        'MsgBox("test worked")
+        Dim psi As New ProcessStartInfo() ' Initialize ProcessStartInfo (psi)
+        psi.Verb = "runas" ' runas = Run As Administrator
+        psi.FileName = "cmd.exe" ' File or exe to run (this cannot take arguments, use ProcessStartInfo.Arguments instead
+        psi.Arguments = "/c " ' Arguments for the process that you're going to run
+        Try
+            Process.Start(psi) ' Run the process (User is required to press Yes to run the program with Administrator access)
+        Catch
+            MsgBox("User cancelled the operation", 16, "") ' User pressed No
+        End Try
     End Sub
 End Class
